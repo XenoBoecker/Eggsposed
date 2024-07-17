51fe7357627e9988cc9a6c42e92fe0b5b3ceb6e0
XenonnoneX <xeno.boecker@web.de>
2024-07-18 00:28:01 +0200

    call VFX

---
+++ Assets/01_Scripts/Chickens/ChickenCallVFX.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ChickenCallVFX : MonoBehaviour
+{
+    Chicken chicken;
+
+
+
+    [SerializeField] SpriteRenderer callSpriteRenderer;
+    [SerializeField] Sprite[] callSprites;
+
+
+    [SerializeField] float callSpriteDuration = 0.3f;
+
+
+    [SerializeField] int totalSpriteRotations = 2;
+
+    private void Start()
+    {
+        chicken = GetComponent<Chicken>();
+        chicken.OnCall += PlayCallVFX;
+
+        callSpriteRenderer.enabled = false;
+    }
+
+    private void PlayCallVFX()
+    {
+        StartCoroutine(AnimateCallSpriteRenderer());
+    }
+
+    IEnumerator AnimateCallSpriteRenderer()
+    {
+        callSpriteRenderer.enabled = true;
+
+        for (int i = 0; i < totalSpriteRotations; i++)
+        {
+            for (int j = 0; j < callSprites.Length; j++)
+            {
+                callSpriteRenderer.sprite = callSprites[j];
+                yield return new WaitForSeconds(callSpriteDuration);
+            }
+        }
+
+        callSpriteRenderer.enabled = false;
+    }
+}
