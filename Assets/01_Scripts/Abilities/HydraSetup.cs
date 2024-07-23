469bf7e65cdd21db8ffceae438e1d366e8b19259
XenonnoneX <xeno.boecker@web.de>
2024-07-23 23:29:11 +0200

    hydra chicken fixed

--- Assets/01_Scripts/Abilities/HydraSetup.cs
+++ Assets/01_Scripts/Abilities/HydraSetup.cs
@@ -4,42 +4,22 @@ public class HydraSetup : ChickenAbilitySetup
 {
     [SerializeField] int callCharges = 3;
 
-    [SerializeField] float callChargeCD = 5;
+    int currentCharges;
 
-    float currentChargeTime;
-
-    protected override void Update()
+    public override void Setup(Chicken chicken)
     {
-        base.Update();
-        
+        base.Setup(chicken);
 
-        if(currentChargeTime < callChargeCD * callCharges) currentChargeTime += Time.deltaTime;
-    }
-
-    public override bool CanCall()
-    {
-        base.CanCall();
-
-        if (currentChargeTime >= callChargeCD) return true;
-        else return false;
+        currentCharges = callCharges;
     }
 
     protected override void SetCallOnCooldown()
     {
-        currentChargeTime -= callChargeCD;
-    }
-
-    protected override void SetChickenCallCDPercentage()
-    {
-        base.SetChickenCallCDPercentage();
-
-        if (currentChargeTime > callChargeCD)
-        {
-            chicken.CurrentCallCooldownPercentage = 1;
-        }
+        if (currentCharges > 1) currentCharges -= 1;
         else
         {
-            chicken.CurrentCallCooldownPercentage = currentChargeTime / callChargeCD;
+            base.SetCallOnCooldown();
+            currentCharges = callCharges;
         }
     }
 }
