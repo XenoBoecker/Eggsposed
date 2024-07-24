3366bc6a9b7a23659b7aebede021076660954151
XenonnoneX <xeno.boecker@web.de>
2024-07-24 13:13:15 +0200

    blood only when on ground

--- Assets/01_Scripts/Abilities/TorturedSetup.cs
+++ Assets/01_Scripts/Abilities/TorturedSetup.cs
@@ -5,16 +5,27 @@ public class TorturedSetup : ChickenAbilitySetup
 
     [SerializeField] GameObject bloodEffect;
 
+    ParticleSystem bloodEffectParticles;
+
     public override void Setup(Chicken chicken)
     {
         base.Setup(chicken);
 
-        Instantiate(bloodEffect, transform);
+        bloodEffectParticles = Instantiate(bloodEffect, transform).GetComponentInChildren<ParticleSystem>();
     }
 
     protected override void Update()
     {
         base.Update();
+
+        if(movement.isGrounded && !bloodEffectParticles.isPlaying)
+        {
+            bloodEffectParticles.Play();
+        }
+        else if (!movement.isGrounded && bloodEffectParticles.isPlaying)
+        {
+            bloodEffectParticles.Stop();
+        }
 
 
         if (CanCall())
